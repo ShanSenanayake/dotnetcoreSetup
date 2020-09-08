acd9202af02184c2b693263aaf79975f07920b85
Shan Senanayake <shan.senanayake@jayway.com>
2020-09-08 13:09:32 +0200

    added a sln file and test project

--- src/kjuge/Startup.cs
+++ src/kjuge/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebAppSetup
+namespace kjuge
 {
     public class Startup
     {
